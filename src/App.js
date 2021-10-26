87ea4e7b885258ef7595af392a4c890e8c28b281
hector6921 <hector_6921@hotmail.com>
2021-10-26 13:51:59 -0400

    added repo

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Blog from './blog';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Blog/>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My Header
+        </p>
+      </header>
+      <div id="body">
+      <p>
+          My Body
+        </p>
+      </div>
+      <div id="footer">
+      <p>
+          My Footer
+        </p>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
